5226a81c68e53c72f231297d543254bfbffe0fe9
Juliette3529 <Juliette@saint-jalme.fr>
2016-12-06 10:27:37 +0100

    setter getter service

--- src/AppBundle/Entity/services.php
+++ src/AppBundle/Entity/services.php
@@ -27,4 +27,40 @@ class Service
      */
     protected $statut;
     
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $title;
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    public function setLevel($title)
+    {
+        $this->level = $level;
+        return $level;
+    }
+    public function getLevel()
+    {
+        return $this->level;
+    }
+    public function setCategorie($categorie)
+    {
+        $this->categorie = $categorie;
+        return $categorie;
+    }
+    public function getCategorie()
+    {
+        return $this->categorie;
+    }
+    public function setTitle($statut)
+    {
+        $this->statut = $statut;
+        return $statut;
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
 }
